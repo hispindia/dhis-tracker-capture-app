93e5a8a81f0a171bf4d200de4d5845d2b4736251
Mithilesh Kumar Thakur <mithilesh.hisp@gmail.com>
2022-03-23 18:00:50 +0530

    disabled/enabled Save button and tei-attribute fields

--- components/registration/registration-controller.js
+++ components/registration/registration-controller.js
@@ -1320,15 +1320,15 @@ trackerCapture.controller('RegistrationController',
     }
 
     $scope.saveAttributedDisabledButton = function(){
-        if($scope.selectedOrgUnit && $scope.selectedOrgUnit.id !== $scope.selectedTei.orgUnit && $scope.registrationMode === 'PROFILE') return true;
-        if($scope.isDisabled(attribute)) return true;
+        //if($scope.selectedOrgUnit && $scope.selectedOrgUnit.id !== $scope.selectedTei.orgUnit && $scope.registrationMode === 'PROFILE') return true;
+        //if($scope.isDisabled(attribute)) return true;
         if($scope.selectedOrgUnit.closedStatus) return true;
         if(!$scope.hasTeiWrite()) return true;
         return false;
     }
 
     $scope.attributeFieldDisabled = function(attribute){
-        if($scope.selectedOrgUnit && $scope.selectedOrgUnit.id !== $scope.selectedTei.orgUnit && $scope.registrationMode === 'PROFILE') return true;
+        //if($scope.selectedOrgUnit && $scope.selectedOrgUnit.id !== $scope.selectedTei.orgUnit && $scope.registrationMode === 'PROFILE') return true;
         if($scope.isDisabled(attribute)) return true;
         if($scope.selectedOrgUnit.closedStatus) return true;
         if(!$scope.hasTeiWrite()) return true;
